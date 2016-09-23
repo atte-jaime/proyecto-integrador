0f144aef301cc16201126d131a8cfa1c87f7a11d
Juan Pablo Gallego <jpgarias09@gmail.com>
2016-09-23 00:20:37 -0500

    sorry c:

---
+++ src/Lista.java
@@ -0,0 +1,18 @@
+
+public class Lista {
+	
+	private int posX, posY;
+	private String name;
+	
+	public Lista(){
+		
+	}
+	
+	public void pintar(int x,int y){
+		
+	}
+	
+	public void cargar(){
+		
+	}
+}
